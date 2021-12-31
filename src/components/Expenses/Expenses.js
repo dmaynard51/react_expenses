7b516f1e0520f25215272656f08c761428d247e6
Daniel Maynard <daniel.maynard83@gmail.com>
2021-12-31 10:43:27 -0800

    add expense

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,49 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import ExpenseItem from './ExpenseItem';
-import Card from '../UI/Card';
-import './Expenses.css';
-import ExpensesFilter from './ExpenseFilter';
+//import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpenseFilter";
+import ExpenseList from "./ExpensesList";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
-    
+  const [filteredYear, setFilteredYear] = useState("2020");
 
-  const saveFilterHandler = (enteredFilter) => { 
-    
+  const saveFilterHandler = (enteredFilter) => {
     setFilteredYear(enteredFilter);
     console.log(filteredYear);
   };
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+
 
   return (
     <div>
-      
-    
-    <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onSaveFilter={saveFilterHandler}/>      
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onSaveFilter={saveFilterHandler}
+        />
+        <ExpenseList items={filteredExpenses}/>
+      </Card>
     </div>
   );
-}
+};
 
 export default Expenses;
